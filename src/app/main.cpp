bbe1c5b0e140c368f8d7ea01d75e4c8363a9cae1
eliogovea <eliogovea1993@gmail.com>
2019-04-30 15:29:53 -0400

    README

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,26 +5,11 @@
 #include <motion_detection/motion_detection.h>
 
 int main() {
-    std::cout << "starting ...\n";
-    // auto reader = std::make_unique<motion_detection::RtspReader>("");
-    // reader->start();
-
     motion_detection::Detector detector{};
 
     // start reader on a different thread
     motion_detection::RtspReader reader{"", &detector};
     std::thread read(&motion_detection::RtspReader::start, &reader);
-
-//    cv::namedWindow("original", cv::WINDOW_AUTOSIZE);
-//    while (true) {
-//        auto frame = reader.getFrame();
-//        if (frame.empty()) {
-//            continue;
-//        }
-//        cv::imshow("original", frame);
-//        cv::waitKey(25);
-//    }
-
 
     read.join();
 }
